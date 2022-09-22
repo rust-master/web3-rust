0570a910c895346d5d25a15b0b04bd5353cc8f65
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2022-09-22 12:15:53 +0500

    TDD

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,22 @@ async fn main() -> web3::Result<()> {
     Ok(())
 }
 
+fn pow(base: i64, exponent: usize) -> i64 {
+    let mut res = 1;
+       if exponent == 0 {
+           return 1;
+       }
+       for _ in 0..exponent {
+           res *= base as i64;
+       }
+    res
+}
 
 #[cfg(test)]
 mod tests {
+    use super::pow;
     #[test]
-    fn it_works() {
-        assert_eq!(2+2, 4);
+    fn minus_two_raised_three_is_minus_eight() {
+        assert_eq!(pow(-2, 3), -8);
     }
 }
